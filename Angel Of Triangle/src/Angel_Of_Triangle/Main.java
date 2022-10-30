9d398b34b50c9c1dd8583ec048fecca3d13317dc
shipraahuja <72993053+shipraahuja@users.noreply.github.com>
2022-10-31 00:53:51 +0530

    Update Main.java

--- Angel Of Triangle/src/Angel_Of_Triangle/Main.java
+++ Angel Of Triangle/src/Angel_Of_Triangle/Main.java
@@ -19,16 +19,8 @@ public class Main {
         x3 = input.nextDouble();
         System.out.println("Enter the vlaue of y3:");
         y3 = input.nextDouble();
-        a = Math.toDegrees(Math.sqrt((Math.pow((x3 - x2), 2)) + (Math.pow((y3 - y2), 2))));
-        b = Math.toDegrees(Math.sqrt((Math.pow((x3 - x1), 2)) + (Math.pow((y3 - y1), 2))));
-        c = Math.toDegrees(Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2))));
-        a1 = (Math.acos((a * a - b * b - c * c) / (-2 * b * c)));
-        a2 = (Math.acos((b * b - a * a - c * c) / (-2 * a * c)));
-        a3 = (Math.acos((c * c - b * b - a * a) / (-2 * a * b)));
-        System.out.println("Angle of A:" + a1);
         System.out.println("Angle of B:" + a2);
         System.out.println("Angle of C:" + a3);
-        if (a1 > a2 && a1 > a3) {
             System.out.println("Maximum A");
         } else if (a2 > a1 && a2 > a3) {
             System.out.println("Maximum B");
